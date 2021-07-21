7825ab725d18092d2830e7dc58eba0805863a404
YuriiChekh <yurii.chekh.sa.2018@lpnu.ua>
2021-07-22 01:57:15 +0300

    init redux_lection_1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import {useDispatch, useSelector} from "react-redux";
+import {useEffect} from "react";
 
+const NestedChild = () =>{
+const counter = useSelector(({counter: {value}})=>value)
+    const fetchPosts =async ()=> {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const json = await response.json();
+        console.log(json);
+    }
+useEffect(()=>{
+    fetchPosts();
+} )
+
+  return(
+      <header className="App-header">
+          <h1>{counter}</h1>
+
+      </header>
+  )
+}
 function App() {
+    const  dispatch = useDispatch()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <button onClick={()=>{
+            dispatch({type:'INC'})
+        }
+        }>inc</button>
+        <button onClick={()=>{
+            dispatch({type:'DEC'})
+        }
+        }>dec</button>
+     <NestedChild/>
+
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,56 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+
+const initialState ={
+    counter: {
+        value:0
+    }
+}
+
+const reducer  = (state = initialState, action) => {
+
+    switch (action.type) {
+        case 'INC':{
+            return {
+                ...state,
+                counter:{
+                    value: state.counter.value + 1
+                }
+
+            }
+
+        }
+        case 'DEC':{
+            return {
+                ...state,
+                counter:{
+                    value: state.counter.value - 1
+                }
+
+            }
+
+        }
+        default:
+            return state;
+    }
+}
+
+const store = createStore(reducer)
+
+
+const INCREMENT_ACTION = {
+    type:'INC'
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <App />
+
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
